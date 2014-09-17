d3455173baf437e40581e3267c50238c6da0499d
Alexey <iamfullofspam@gmail.com>
2014-09-17 20:54:07 +0400

    merging two branches

--- linear_transform.py
+++ linear_transform.py
@@ -1,1 +1,129 @@
-__author__ = 'axelr'
+import numpy
+import pylab
+from sklearn.neighbors import NearestNeighbors
+from sklearn.utils.validation import check_arrays
+
+__author__ = 'Alex Rogozhnikov'
+
+
+def exp_potential(r):
+    return numpy.exp(-r)
+
+
+def power_potential(power=-1, lower=0.02):
+    def _potential(r):
+        return numpy.clip(r, lower, 1e10) ** power
+    return _potential
+
+
+def plot_data(X, y):
+    for label in set(y):
+        pylab.plot(X[y == label, 0], X[y == label, 1], '.')
+    pylab.show()
+
+
+def generate_linear_transform(X, y, potential=exp_potential,
+                              iterations=10, n_neighbors=10, r_neighbours=2., eps=0.05,
+                              max_class_data_on_iteration=200,
+                              penalize_same_class=True, balance_forces=True):
+    """
+    This function should return some linear transform, that enlarges the distances between classes
+    :return:
+    """
+    X, y = check_arrays(X, y)
+    n_dimensions = X.shape[1]
+    y = numpy.array(y, dtype=int)
+    labels = set(y)
+    X_init = numpy.copy(X)
+
+    resulting_transform = numpy.identity(n_dimensions)
+
+    for iteration in range(iterations):
+        nn = NearestNeighbors(n_neighbors=n_neighbors, radius=r_neighbours)
+        indices = []
+        for label in labels:
+            ind = numpy.where(y == label)[0]
+            numpy.random.shuffle(ind)
+            indices.append(ind[:max_class_data_on_iteration])
+
+        indices = numpy.concatenate(indices)
+        assert len(indices) == len(set(indices))
+        X_iter = X[indices, :]
+        y_iter = y[indices]
+
+        nn.fit(X_iter)
+        knn_indices = nn.kneighbors(X_iter, n_neighbors=n_neighbors, return_distance=False)
+        knn_indices = knn_indices[:, 1:]
+
+        # in each row two close events
+        neighbors = numpy.zeros([knn_indices.shape[0] * knn_indices.shape[1], 2], dtype=numpy.int)
+        i = 0
+        for index1, knn_1 in zip(range(len(X_iter)), knn_indices):
+            for index2 in knn_1:
+                neighbors[i] = [index1, index2]
+                i += 1
+
+        # each row is vector x_i - x_j, where i and j are neighbouring events
+        pairwise_vectors = numpy.zeros([len(neighbors), X.shape[1]])
+        same_class = numpy.zeros(len(neighbors), dtype=numpy.int)
+
+        for i, (index1, index2) in enumerate(neighbors):
+            same_class[i] = y_iter[index1] == y_iter[index2]
+            pairwise_vectors[i] = X_iter[index1, :] - X_iter[index2, :]
+
+        # positive for different classes
+        same_class_multiplier = 1 - same_class
+        if penalize_same_class:
+            same_class_multiplier = 1 - 2 * same_class
+
+        r = numpy.linalg.norm(pairwise_vectors, axis=1)
+        assert len(r) == len(pairwise_vectors)
+
+        multiplier = same_class_multiplier * potential(r)
+
+        if balance_forces:
+            multiplier[multiplier > 0] /= numpy.sum(multiplier[multiplier > 0])
+            multiplier[multiplier < 0] /= - numpy.sum(multiplier[multiplier < 0])
+
+        derivatives = numpy.dot(pairwise_vectors.T,  multiplier[:, numpy.newaxis] * pairwise_vectors)
+        derivatives /= numpy.linalg.norm(derivatives, ord='fro')
+
+        # print numpy.linalg.norm(derivatives, ord='fro')
+
+        transformer = numpy.identity(n_dimensions, dtype=numpy.float64) + derivatives * eps
+        transformer *= numpy.linalg.det(transformer) ** (- 1. / n_dimensions)
+
+        assert numpy.all(numpy.isfinite(transformer))
+        assert 0.99 < numpy.linalg.det(transformer) < 1.01
+
+        resulting_transform = numpy.dot(resulting_transform, transformer)
+        X = numpy.dot(X, transformer)
+        assert numpy.allclose(X, numpy.dot(X_init, resulting_transform))
+        yield resulting_transform
+
+
+def sandwich_data(num_classes=6, num_points=22, dist=0.4):
+    # memory pre-allocation
+    x = numpy.zeros([num_classes * num_points, 2])
+    y = numpy.zeros(num_classes * num_points, dtype=int)
+    for i in xrange(num_classes):
+        y[i * num_points: i * num_points + num_points] = i
+        for j in xrange(num_points):
+            x[i * num_points + j, :] = numpy.array([numpy.random.normal(j, 0.1), numpy.random.normal(dist * i, 0.1)])
+    return x, y
+
+
+def gauss_data(n_samples=1000, n_features=10, classes=5, std=1.):
+    from sklearn.datasets import make_blobs
+    return make_blobs(n_samples=n_samples, n_features=n_features, cluster_std=std, centers=classes)
+
+
+
+
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,1 +1,110 @@
-__author__ = 'axelr'
+# About
+
+# This module contains some helpful and commonly used functions
+from __future__ import print_function
+from __future__ import division
+
+import itertools
+
+import numpy
+import pandas
+import pylab
+import root_numpy
+import sklearn
+from sklearn.neighbors.unsupervised import NearestNeighbors
+from sklearn.base import TransformerMixin, BaseEstimator
+import os
+
+
+
+__author__ = 'Alex Rogozhnikov'
+
+
+def get_file_labels(labels, folder='/mnt/w76/notebook/datasets/doudko/samples/'):
+    file_labels = {}
+    for name in os.listdir(folder):
+        for i, label in enumerate(labels):
+            if label in name:
+                file_labels[name] = i
+    return file_labels
+
+
+def load_data(file_labels, variables, folder='/mnt/w76/notebook/datasets/doudko/samples/', weight_column='weight'):
+    all_variables = variables + [weight_column]
+    data_parts = []
+    data_labels = []
+    data_weights = []
+    for filename, label in file_labels.iteritems():
+        data = root_numpy.root2array(folder + filename, treename='Vars', branches=all_variables)
+        data = pandas.DataFrame(data)
+        data_labels.append(numpy.zeros(len(data), dtype=int) + label)
+        data_weights.append(data[weight_column])
+        data_parts.append(data.drop([weight_column], axis=1))
+
+    data = pandas.concat(data_parts, ignore_index=True)
+    labels = numpy.concatenate(data_labels)
+    weights = numpy.concatenate(data_weights)
+    return data, labels, weights
+
+
+def check_sample_weight(y_true, sample_weight):
+    """Checks the weights, returns normalized version """
+    if sample_weight is None:
+        return numpy.ones(len(y_true), dtype=numpy.float)
+    else:
+        sample_weight = numpy.array(sample_weight, dtype=numpy.float)
+        assert len(y_true) == len(sample_weight), \
+            "The lengths are different: {0} and {1}".format(len(y_true), len(sample_weight))
+        return sample_weight
+
+
+def distance_quality_matrix(X, y, n_neighbors=50):
+    """On of the ways to measure the quality of knning"""
+    labels = numpy.unique(y)
+    nn = NearestNeighbors(n_neighbors=n_neighbors)
+    nn.fit(X)
+    knn_indices = nn.kneighbors(X, n_neighbors=n_neighbors, return_distance=False)
+    confusion_matrix = numpy.zeros([len(labels), len(labels)], dtype=int)
+    for label1, labels2 in zip(y, numpy.take(y, knn_indices)):
+        for label2 in labels2:
+            confusion_matrix[label1, label2] += 1
+
+    return confusion_matrix
+
+
+def plot_confusion_matrix(matrix):
+    pylab.figure(figsize=(16, 4))
+    pylab.subplot(131), pylab.title('Original')
+    matrix = numpy.array(matrix, dtype=float)
+    pylab.pcolor(matrix / matrix.sum(axis=None)), pylab.colorbar()
+    pylab.subplot(132), pylab.title('Normed by row')
+    pylab.pcolor(matrix / matrix.sum(axis=1, keepdims=True)), pylab.colorbar()
+    pylab.subplot(133), pylab.title('Normed by column')
+    pylab.pcolor(matrix / matrix.sum(axis=0, keepdims=True)), pylab.colorbar()
+
+
+class FeatureGenerator(BaseEstimator, TransformerMixin):
+    def __init__(self, base_estimator):
+        self.base_estimator = sklearn.clone(base_estimator)
+
+    def fit(self, X, y, sample_weight):
+        sample_weight = check_sample_weight(y, sample_weight=sample_weight)
+        self.estimators = dict()
+        labels = numpy.unique(y)
+        X = numpy.array(X)
+        for label1, label2 in itertools.combinations(labels, 2):
+            mask = numpy.in1d(y, [label1, label2])
+            clf = sklearn.clone(self.base_estimator)
+            clf.fit(X[mask, :], y[mask], sample_weight=sample_weight[mask])
+            self.estimators[(label1, label2)] = clf
+            print('trained', label1, 'vs', label2)
+
+    def transform(self, X, keep_original_features=True):
+        if keep_original_features:
+            result = pandas.DataFrame.copy(X)
+        else:
+            result = pandas.DataFrame()
+        for (label1, label2), clf in self.estimators.iteritems():
+            result['trained%ivs%i' % (label1, label2)] = clf.predict_proba(X)[:, 1]
+
+        return result
