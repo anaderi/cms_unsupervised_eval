565d975d220d3eccc45468d781300ccf07b9696f
Alexey <iamfullofspam@gmail.com>
2014-10-25 03:38:27 +0400

    updated utils, added feture generation

--- utils.py
+++ utils.py
@@ -1,12 +1,50 @@
 # About
 
 # This module contains some helpful and commonly used functions
+from __future__ import print_function
+from __future__ import division
+
+import itertools
 
 import numpy
+import pandas
 import pylab
+import sklearn
 from sklearn.neighbors.unsupervised import NearestNeighbors
+from sklearn.base import TransformerMixin, BaseEstimator
+import os
+
+
 
 __author__ = 'Alex Rogozhnikov'
+
+
+def get_file_labels(labels, folder='/mnt/w76/notebook/datasets/doudko/samples/'):
+    file_labels = {}
+    for name in os.listdir(folder):
+        for i, label in enumerate(labels):
+            if label in name:
+                file_labels[name] = i
+    return file_labels
+
+
+def load_data(file_labels, variables, folder='/mnt/w76/notebook/datasets/doudko/samples/', weight_column='weight'):
+    import root_numpy
+    all_variables = variables + [weight_column]
+    data_parts = []
+    data_labels = []
+    data_weights = []
+    for filename, label in file_labels.iteritems():
+        data = root_numpy.root2array(folder + filename, treename='Vars', branches=all_variables)
+        data = pandas.DataFrame(data)
+        data_labels.append(numpy.zeros(len(data), dtype=int) + label)
+        data_weights.append(data[weight_column])
+        data_parts.append(data.drop([weight_column], axis=1))
+
+    data = pandas.concat(data_parts, ignore_index=True)
+    labels = numpy.concatenate(data_labels)
+    weights = numpy.concatenate(data_weights)
+    return data, labels, weights
 
 
 def check_sample_weight(y_true, sample_weight):
@@ -43,3 +81,30 @@ def plot_confusion_matrix(matrix):
     pylab.pcolor(matrix / matrix.sum(axis=1, keepdims=True)), pylab.colorbar()
     pylab.subplot(133), pylab.title('Normed by column')
     pylab.pcolor(matrix / matrix.sum(axis=0, keepdims=True)), pylab.colorbar()
+
+
+class FeatureGenerator(BaseEstimator, TransformerMixin):
+    def __init__(self, base_estimator):
+        self.base_estimator = sklearn.clone(base_estimator)
+
+    def fit(self, X, y, sample_weight):
+        sample_weight = check_sample_weight(y, sample_weight=sample_weight)
+        self.estimators = dict()
+        labels = numpy.unique(y)
+        X = numpy.array(X)
+        for label1, label2 in itertools.combinations(labels, 2):
+            mask = numpy.in1d(y, [label1, label2])
+            clf = sklearn.clone(self.base_estimator)
+            clf.fit(X[mask, :], y[mask], sample_weight=sample_weight[mask])
+            self.estimators[(label1, label2)] = clf
+            print('trained', label1, 'vs', label2)
+
+    def transform(self, X, keep_original_features=True):
+        if keep_original_features:
+            result = pandas.DataFrame.copy(X)
+        else:
+            result = pandas.DataFrame()
+        for (label1, label2), clf in self.estimators.iteritems():
+            result['trained%ivs%i' % (label1, label2)] = clf.predict_proba(X)[:, 1]
+
+        return result
