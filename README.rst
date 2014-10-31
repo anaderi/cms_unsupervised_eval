4ce6977ca2e01bde2f271b0809288e5903786a28
Alexey <iamfullofspam@gmail.com>
2014-11-01 02:12:09 +0300

    created notebooks folder, prepared setup.py

---
+++ README.rst
@@ -0,0 +1,12 @@
+===================================
+CMS unsupervised machine learning (CMSUML)
+===================================
+
+The project is intended to find good features / distance functions for the data from CMS experiment.
+This set of features should discriminate different decay channels.
+
+* not only supervised, we are using toyMC with answers to check our guesses.
+
+We are using IPython for analyses (an interactive shell for python language)
+
+Tested under python 2.7
---
+++ setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup
+import codecs
+
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="cmsuml",
+    version=0.1,
+    description="Unsupervised methods of machine learning in CMS",
+    long_description=long_description,
+
+    url='https://github.com/anaderi/cms_unsupervised_eval',
+
+    # Author details
+    author='Alex Rogozhnikov',
+    author_email='antares@yandex-team.ru',
+
+    # Choose your license
+    license='Yandex',
+    packages=['cmsuml'],
+    package_dir={'cmsuml': 'cmsuml'},
+    classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: CERN, CMS, HEP',
+        'Topic :: YDF :: Cern Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: YDF :: Yandex License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='machine learning, unsupervised learning, high energy physics ',
+
+    # List run-time dependencies here. These will be installed by pip when your
+    # project is installed.
+    install_requires = [
+        'ipython >= 2.1.0',
+        'pyzmq >= 14.3.0',
+        'matplotlib >= 1.4',
+        'rootpy >= 0.7.1',
+        'root_numpy >= 3.3.0',
+        'pandas >= 0.14.0',
+        'scikit-learn >= 0.15',
+        'scipy >= 0.14.0',
+        'numpy >= 1.8.1',
+        'jinja2 >= 2.7.3',
+    ],
+)
